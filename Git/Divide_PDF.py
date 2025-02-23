d0de39b37d47272eac31c4a72ab6a7c1f0bee92f
Luis Vasconcelos <vasconcelos.luisgs@gmail.com>
2025-02-23 15:51:32 -0300

    Adição de novos documentos e notebooks, além de ajustes em scripts de manipulação de PDF

--- Git/Divide_PDF.py
+++ Git/Divide_PDF.py
@@ -8,7 +8,7 @@ if not os.path.isfile(file_name):
 else:
     inputpdf = PdfReader(open(file_name, "rb"))
 
-    for i in range(83, 104):
+    for i in range(301, 332):
         writer = PdfWriter()
         writer.add_page(inputpdf.pages[i])
         with open("%s-page%s.pdf" % (file_name, i), "wb") as output_pdf:
@@ -17,7 +17,7 @@ else:
     # Merge the split PDF files
     merge_pdf = PdfMerger()
 
-    for i in range(83, 104):
+    for i in range(301, 332):
         merge_pdf.append(open("%s-page%s.pdf" % (file_name, i), "rb"))
 
     with open("merged_%s" % file_name, "wb") as output_pdf:
